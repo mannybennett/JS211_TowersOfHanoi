c91c361e20bfec8ca6f28567511a018a3647d6d4
mannybennett <mannybennett99@gmail.com>
2023-01-19 15:06:55 -0500

    beginning

--- main.js
+++ main.js
@@ -50,7 +50,8 @@ const checkForWin = () => {
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
-
+  // movePiece()
+  // checkForWin()
 }
 
 const getPrompt = () => {
